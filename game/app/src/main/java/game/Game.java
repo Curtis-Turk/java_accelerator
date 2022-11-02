e2c6b8a830bdc723d7261cc17e7923617007688e
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-02 16:16:10 +0000

    Refactored the wordtoguess function to hide word

--- game/app/src/main/java/game/Game.java
+++ game/app/src/main/java/game/Game.java
@@ -2,16 +2,25 @@ package game;
 
 public class Game {
   String inputWord;
+  String hiddenWord;
 
   Game(String word) {
     inputWord = word;
   }
 
-  String hideWord() {
+  public String hideWord() {
+
     return inputWord;
   }
 
   public String getWordToGuess(String word) {
-    return word;
+
+    StringBuilder sb = new StringBuilder(word);
+
+    for (int i = 1; i < word.length(); i++) {
+      sb.replace(i, word.length(), "_");
+    }
+
+    return sb.toString();
   }
 }
--- game/app/src/test/java/game/GameTest.java
+++ game/app/src/test/java/game/GameTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 public class GameTest {
   @Test
   public void testGetsWordToGuess() {
-    String word = "B_____";
+    String word = "BOOKS";
     Game game = new Game(word);
-    assertEquals(game.getWordToGuess(word), "B_____");
+    assertEquals(game.getWordToGuess(word), "B____");
   }
 
   @Test
   public void makersWordToGuess() {
     String word = "MAKERS";
     Game game = new Game(word);
-    assertEquals(game.getWordToGuess(word), "MAKERS");
+    assertEquals(game.getWordToGuess(word), "M_____");
   }
 }
