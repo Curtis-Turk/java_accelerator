95e969d642dc89d82221521486675eeb72b20d52
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-04 17:52:40 +0000

    Added dependency injection

--- shopping/app/src/test/java/shopping/ShoppingListTest.java
+++ shopping/app/src/test/java/shopping/ShoppingListTest.java
@@ -1,5 +1,21 @@
 package shopping;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+
 public class ShoppingListTest {
 
+  @Test
+  public void initialsesWithEmptyList() {
+    ShoppingList shopList = new ShoppingList();
+    Assert.assertEquals(0, shopList.getItems().size());
+  }
+
+  @Test
+  public void canAddItemsToList() {
+    ShoppingList shopList = new ShoppingList();
+    shopList.addItem(new ShoppingItem("Tofu", 4.99));
+    Assert.assertEquals("Adds something to the list", 1, shopList.getItems().size());
+  }
 }
