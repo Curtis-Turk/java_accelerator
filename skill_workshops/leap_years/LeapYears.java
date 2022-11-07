423df44266bd60aeeec071332184f3e2160a400d
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-07 15:50:39 +0000

    Updated code with fully tested response

--- skill_workshops/leap_years/LeapYears.java
+++ skill_workshops/leap_years/LeapYears.java
@@ -1,5 +1,21 @@
-public class LeapYears {
-  public Boolean isLeapYear(int num){
-    return true;
-  }
+package leap_years_gradle;
+import java.util.ArrayList;
+
+public class LeapYears{
+    public Boolean isLeapYear(int inputYear){
+        if(inputYear <= 0){
+            return false;
+        }
+        return (inputYear % 4 == 0 && inputYear % 100 != 0) || (inputYear % 400 == 0);
+    }
+    public ArrayList<Integer> allLeapYears(int yearFrom, int yearTo){
+        ArrayList<Integer> leapYears = new ArrayList<Integer>();
+        for(int i = yearFrom; i <= yearTo; i++){
+           if(isLeapYear(i)){
+               leapYears.add(i);
+           }
+        }
+        return leapYears;
+    }
 }
+
