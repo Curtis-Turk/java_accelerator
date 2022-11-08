c571acc5226e1d3b19861451bc2b7c7b80239b7f
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-08 12:49:06 +0000

    Small refactoring

--- skill_workshops/10_minute_walk/app/src/main/java/_minute_walk/TenMinWalk.java
+++ skill_workshops/10_minute_walk/app/src/main/java/_minute_walk/TenMinWalk.java
@@ -2,7 +2,6 @@ package _minute_walk;
 
 public class TenMinWalk {
     public Boolean ten_minute_walk(String[] walk) {
-        if(walk.length != 10) return false;
         int northSouth = 0;
         int eastWest = 0;
         for (String s : walk) {
@@ -11,6 +10,6 @@ public class TenMinWalk {
             if (s.equals("E")) eastWest++;
             if (s.equals("W")) eastWest--;
         }
-        return (northSouth - eastWest) == 0;
+        return (northSouth - eastWest) == 0 && walk.length == 10;
     }
 }
