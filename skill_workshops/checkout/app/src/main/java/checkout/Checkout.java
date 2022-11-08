ca6c647e661742b03cdf66321dfac1cf342e9e33
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-08 16:55:47 +0000

    Passing tests for deals on As and Bs

--- skill_workshops/checkout/app/src/main/java/checkout/Checkout.java
+++ skill_workshops/checkout/app/src/main/java/checkout/Checkout.java
@@ -3,15 +3,31 @@ package checkout;
 public class Checkout {
     public Integer checkout(String inputSKUs){
         Integer total = 0;
+        Integer aCount = 0;
+        Integer bCount = 0;
         for (int i = 0; i < inputSKUs.length(); i++) {
-           if(inputSKUs.charAt(i) == 'A'){
-               total += 50;
-           }else if(inputSKUs.charAt(i) == 'B') {
-               total += 30;
-           }else if(inputSKUs.charAt(i) == 'C') {
-               total += 20;
-           }else
-               total += 15;
+            if (inputSKUs.charAt(i) == 'A') {
+                aCount++;
+                total += 50;
+            } else if (inputSKUs.charAt(i) == 'B') {
+                bCount++;
+                total += 30;
+            } else if (inputSKUs.charAt(i) == 'C') {
+                total += 20;
+            } else if (inputSKUs.charAt(i) == 'D') {
+                total += 15;
+            } else
+                return -1;
+        }
+        if(aCount > 2) {
+            for (int i = 1; i < aCount; i += 3) {
+                total -= 20;
+            }
+        }
+        if(bCount > 1) {
+            for (int i = 1; i < bCount; i += 2) {
+                total -= 15;
+            }
         }
         return total;
     }
--- skill_workshops/checkout/app/src/test/java/checkout/CheckoutTest.java
+++ skill_workshops/checkout/app/src/test/java/checkout/CheckoutTest.java
@@ -5,34 +5,45 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckoutTest {
+    public static Checkout shop = new Checkout();
     @Test
     public void checkoutA(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("A"), 50);
     }
     @Test
     public void checkoutAA(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("AA"), 100);
     }
     @Test
     public void checkoutB(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("B"), 30);
     }
     @Test
     public void checkoutAB(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("AB"), 80);
     }
     @Test
     public void checkoutC(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("C"), 20);
     }
     @Test
     public void checkoutD(){
-        Checkout shop = new Checkout();
         assertEquals(shop.checkout("D"), 15);
+    }
+    @Test
+    public void errorCheckForNonCapital(){
+        assertEquals(shop.checkout("d"), -1);
+    }
+    @Test
+    public void errorCheckForNonCapitalAtStart(){
+        assertEquals(shop.checkout("daaC"), -1);
+    }
+    @Test
+    public void dealFor3A(){
+        assertEquals(shop.checkout("AAA"), 130);
+    }
+    @Test
+    public void dealFor2B(){
+        assertEquals(shop.checkout("BB"), 45);
     }
 }
