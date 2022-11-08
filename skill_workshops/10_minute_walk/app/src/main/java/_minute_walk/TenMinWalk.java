12f105eca2edd1e1e9583a47e5ef9a944df2013c
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-08 12:43:52 +0000

    Completed exercise

--- skill_workshops/10_minute_walk/app/src/main/java/_minute_walk/TenMinWalk.java
+++ skill_workshops/10_minute_walk/app/src/main/java/_minute_walk/TenMinWalk.java
@@ -2,7 +2,15 @@ package _minute_walk;
 
 public class TenMinWalk {
     public Boolean ten_minute_walk(String[] walk) {
-        if(walk.length != 10)return false;
-        return true;
+        if(walk.length != 10) return false;
+        int northSouth = 0;
+        int eastWest = 0;
+        for (String s : walk) {
+            if (s.equals("N")) northSouth++;
+            if (s.equals("S")) northSouth--;
+            if (s.equals("E")) eastWest++;
+            if (s.equals("W")) eastWest--;
+        }
+        return (northSouth - eastWest) == 0;
     }
 }
--- skill_workshops/10_minute_walk/app/src/test/java/_minute_walk/TenMinWalkTest.java
+++ skill_workshops/10_minute_walk/app/src/test/java/_minute_walk/TenMinWalkTest.java
@@ -25,5 +25,22 @@ public class TenMinWalkTest {
         String[] walk = {"W","E","W","E","W","E","W","E","W","E"};
         assertTrue(tenMinWalk.ten_minute_walk(walk));
     }
-    
+    @Test
+    public void falseWhenWalkIs10minAndDoesNotGoBackToOrigin(){
+        TenMinWalk tenMinWalk = new TenMinWalk();
+        String[] walk = {"W","E","W","E","W","E","W","E","W","N"};
+        assertFalse(tenMinWalk.ten_minute_walk(walk));
+    }
+    @Test
+    public void falseForRandomWalk(){
+        TenMinWalk tenMinWalk = new TenMinWalk();
+        String[] walk = {"W","W","W","E","S","E","W","E","W","N"};
+        assertFalse(tenMinWalk.ten_minute_walk(walk));
+    }
+    @Test
+    public void trueWhenLowerCase(){
+        TenMinWalk tenMinWalk = new TenMinWalk();
+        String[] walk = {"w","e","W","e","W","E","w","E","W","E"};
+        assertTrue(tenMinWalk.ten_minute_walk(walk));
+    }
 }
