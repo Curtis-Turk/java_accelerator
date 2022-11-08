ea849acc542251afae08367d9cf6c9e584d2116c
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-08 16:30:57 +0000

    Passing Test for C

--- skill_workshops/checkout/app/src/main/java/checkout/Checkout.java
+++ skill_workshops/checkout/app/src/main/java/checkout/Checkout.java
@@ -6,8 +6,10 @@ public class Checkout {
         for (int i = 0; i < inputSKUs.length(); i++) {
            if(inputSKUs.charAt(i) == 'A'){
                total += 50;
-           }else
+           }else if(inputSKUs.charAt(i) == 'B') {
                total += 30;
+           }else
+               total += 20;
         }
         return total;
     }
