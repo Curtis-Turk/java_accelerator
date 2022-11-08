d60a6cd0ba1d5c9ac87fa0c0e7552e0067a019c9
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-08 13:17:25 +0000

    Made first test

--- skill_workshops/scrabble_solver/app/src/main/java/scrabble_solver/ScrabbleSolver.java
+++ skill_workshops/scrabble_solver/app/src/main/java/scrabble_solver/ScrabbleSolver.java
@@ -1,4 +1,11 @@
 package scrabble_solver;
 
 public class ScrabbleSolver {
+    char word;
+    ScrabbleSolver(char inputWord){
+        word = inputWord;
+    }
+    public Integer score(){
+        return 1;
+    }
 }
--- skill_workshops/scrabble_solver/app/src/test/java/scrabble_solver/ScrabbleSolverTest.java
+++ skill_workshops/scrabble_solver/app/src/test/java/scrabble_solver/ScrabbleSolverTest.java
@@ -1,4 +1,13 @@
 package scrabble_solver;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ScrabbleSolverTest {
+    @Test
+    public void calculatesA(){
+        ScrabbleSolver scrabbleSolver = new ScrabbleSolver('a');
+        assertEquals(scrabbleSolver.score(), 1);
+    }
 }
